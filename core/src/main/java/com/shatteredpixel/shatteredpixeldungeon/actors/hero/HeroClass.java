6a502b3e2b2ad39a4fde33d4f8473f93274b7a2b
Evan Debenham <Evan@ShatteredPixel.com>
2023-01-26 18:00:11 -0500

    v2.0.0: added various things for the Alpha release

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
@@ -86,7 +86,7 @@ public enum HeroClass {
 	MAGE( HeroSubClass.BATTLEMAGE, HeroSubClass.WARLOCK ),
 	ROGUE( HeroSubClass.ASSASSIN, HeroSubClass.FREERUNNER ),
 	HUNTRESS( HeroSubClass.SNIPER, HeroSubClass.WARDEN ),
-	DUELIST( HeroSubClass.CHAMPION, HeroSubClass.MONK);
+	DUELIST( HeroSubClass.CHAMPION );
 
 	private HeroSubClass[] subClasses;
 
